fc564edbf8a932ff82f1a429b8a7dccc86c5ca08
Callum Wilson <callum@Rebecas-iMac.local>
2020-02-10 14:32:26 +0000

    added map functionality

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -2,30 +2,11 @@ import React from 'react';
 import { createStackNavigator, HeaderTitle, HeaderBackButton } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 import ItemDetails from '../screens/itemDetails';
 import HomeScreen from '../screens/home';
 import PropertyLocation from '../screens/propertyLocation';
-
-const NavStack = createStackNavigator({
-    Home: {
-        screen: HomeScreen,
-        navigationOptions: {
-            title: 'Property List',
-            headerStyle: { backgroundColor: '#0099ff'},
-            headerTintColor: 'white',
-        },
-    },
-    ItemDetails: {
-        screen: ItemDetails,
-        navigationOptions: {
-            title: 'Property Details',
-            headerStyle: { backgroundColor: '#0099ff'},
-            headerTintColor: 'white',
-            headerBackTitle: 'Back'
-        }
-    },
-});
 
 const getActiveRouteState = function (route) {
     if (!route.routes || route.routes.length === 0 || route.index >= route.routes.length) {
@@ -37,20 +18,48 @@ const getActiveRouteState = function (route) {
 }
 
 const TabNavigator = createBottomTabNavigator({
-    NaviStack: {
-        screen: NavStack,
+    ItemDetails: {
+        screen: ItemDetails,
         navigationOptions: {
-            title: 'Details'
+            title: 'Property Details',
+            headerStyle: { backgroundColor: '#0099ff'},
+            headerTintColor: 'white',
+            headerBackTitle: 'Back',
+            tabBarIcon: <Icon name='home'/>
         }
     },
     PropertyLocation: {
         screen: PropertyLocation,
         navigationOptions: {
-            title: 'Location'
+            title: 'Location',
+            headerStyle: { backgroundColor: '#0099ff'},
+            headerTintColor: 'white',
+            HeaderTitle: 'Location',
+            tabBarIcon: <Icon name='navigation'/>
         }
     },
 });
 
-const App = createAppContainer(TabNavigator);
+const NavStack = createStackNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            title: 'Property List',
+            headerStyle: { backgroundColor: '#0099ff'},
+            headerTintColor: 'white',
+        },
+    },
+    TabNavi: {
+        screen: TabNavigator,
+        navigationOptions: {
+            headerStyle: { backgroundColor: '#0099ff'},
+            title: 'Property Details',
+            headerTintColor: 'white',
+            headerBackTitle: 'Back'
+        },
+    },
+});
+
+const App = createAppContainer(NavStack);
 
 export default App;
--- screens/propertyLocation.js
+++ screens/propertyLocation.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 
 
-export default function PropertyLocation() {
+export default function PropertyLocation({ navigation }) {
 
+    var  propertyDetails = navigation.dangerouslyGetParent()._childrenNavigation.ItemDetails.state.params.property;
+
+    //console.log(propertyDetails.displayable_address);
 
     return (
         <View style={styles.container}>
-            <Text>Property Location</Text>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: propertyDetails.latitude,
+                    longitude: propertyDetails.longitude,
+                    latitudeDelta: 0.0522,
+                    longitudeDelta: 0.0221,
+                }}
+                showsUserLocation={true}
+            >
+            <MapView.Marker 
+                coordinate={{latitude: propertyDetails.latitude,
+                    longitude: propertyDetails.longitude }}
+                    title= {propertyDetails.displayable_address}
+            />
+            </MapView>
         </View>
 )
 }
 
+let { height, width } = Dimensions.get('window');
 
 const styles= StyleSheet.create({
 
     container: {
         padding: 24,
+        alignItems: "center",
+        height: height,
+        width: width
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
     },
 });
